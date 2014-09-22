9872eb1665fc0f7840be0061a65f20b4d8ac725a
Bernard Mordan <bernardmordan@gmail.com>
2014-09-22 10:06:58 +0100

    say functionality added so ruby pairs.rb will say (on a mac)

--- lib/pairs.rb
+++ lib/pairs.rb
@@ -1,5 +1,10 @@
+say = 'say -v fiona --rate=20 '
+
 names = %w[ Alex AndrewHe AndrewHa AndrewSn Ana Anna Bernard Camilla Colin Craig Dan Denise Elena Ella Fadie Hadi James Karin Nicole Pablo Rachel Sandrine Sean Steve Tim Victoria Yvette Zeeshan]
 
 new_names = names.shuffle
 
-14.times {system say+"#{new_names.pop} and #{new_names.pop}"}
+14.times do
+  system say+new_names.pop+' pair with '+new_names.pop
+  sleep 1
+end
