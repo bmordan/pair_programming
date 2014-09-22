056f295e13e50b7d8fc0a1cc27554570a8d9af25
Stephen Giles <s.giles@hotmail.co.uk>
2014-09-22 09:12:01 +0100

    14 pairs generated. Basic.

---
+++ lib/pairs.rb
@@ -0,0 +1,5 @@
+names = %w[ Alex AndrewHe AndrewHa AndrewSn Ana Anna Bernard Camilla Colin Craig Dan Denise Elena Ella Fadie Hadi James Karin Nicole Pablo Rachel Sandrine Sean Steve Tim Victoria Yvette Zeeshan]
+
+new_names = names.shuffle
+
+14.times {puts "#{new_names.pop} and #{new_names.pop}"}
