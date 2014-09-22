58842fc2907c0cb335023978ce80719f3df230ab
Bernard Mordan <bernardmordan@gmail.com>
2014-09-22 11:44:23 +0100

    surnames and putting to console

--- lib/pairs.rb
+++ lib/pairs.rb
@@ -1,10 +1,13 @@
 say = 'say -v fiona --rate=20 '
 
-names = %w[ Alex AndrewHe AndrewHa AndrewSn Ana Anna Bernard Camilla Colin Craig Dan Denise Elena Ella Fadie Hadi James Karin Nicole Pablo Rachel Sandrine Sean Steve Tim Victoria Yvette Zeeshan]
+names = %w[ Alex AndrewHerculus AndrewHastings AndrewSnead Ana AnnaJ Bernard Camilla Colin Craig Dan Denise Elena Ella Fadie Hadi James Karin Nicole Pablo Rachel Sandrine Sean Steve Tiiiiiim Victoria Yvette Zeeshan]
 
 new_names = names.shuffle
 
 14.times do
-  system say+new_names.pop+' pair with '+new_names.pop
+  pairs = "#{new_names.pop} pairs with #{new_names.pop}"
+  puts pairs
+  system("say #{pairs}")
   sleep 1
 end
+
